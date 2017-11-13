57eaf5aecd011eea95697f50b06bf2a34d2c3bce
Trojan-Hawk <G00333333@gmit.ie>
2017-11-13 20:44:28 +0000

    ElizaResponse function added

--- GoProblems3.go
+++ GoProblems3.go
@@ -1,6 +1,10 @@
 // Student Name: Timothy Cassidy
 // Student Number: G00333333
 
+// Resources
+// https://golang.org/pkg/math/rand/#Rand.Intn
+// 
+
 package main
 
 // format package import
@@ -13,11 +17,18 @@ var responseItems = []string {
 	"I'm not sure what you're trying to say. Could you explain it to me?",
 	"How does that make you feel?",
 	"Why do you say that?"
+}// responseItems
+
+// function that takes a string and returns a response string
+func ElizaResponse(inputString string) string {
+	// Intn returns, as an int, a non-negative pseudo-random number
+	// using this number to randomly pick a response from the array
+	return responseItems[rand.Intn(len(responseItems))]
 }
 
 // Hello World main function
 func main() {
-	// random seed generator
+	// using the system clock to set a random seed value
 	rand.Seed(time.Now().UTC().UnixNano())
 	
 	
