9a706b3ef23f5cd5f242f51a2abaa9fb9a35e125
Trojan-Hawk <G00333333@gmit.ie>
2017-11-13 20:37:07 +0000

    Generic response variable and random number

--- GoProblems3.go
+++ GoProblems3.go
@@ -5,9 +5,20 @@ package main
 
 // format package import
 import "fmt"
+import "time"
+import "math/rand"
+
+// generic responses string array
+var responseItems = []string {
+	"I'm not sure what you're trying to say. Could you explain it to me?",
+	"How does that make you feel?",
+	"Why do you say that?"
+}
 
 // Hello World main function
 func main() {
+	// random seed generator
+	rand.Seed(time.Now().UTC().UnixNano())
 	
 	
 	
