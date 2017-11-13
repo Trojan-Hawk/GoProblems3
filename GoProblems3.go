d8d9e7d4e2fe847178e45a3f3c6ea67141006845
Trojan-Hawk <G00333333@gmit.ie>
2017-11-13 19:47:17 +0000

    First Commit

---
+++ GoProblems3.go
@@ -0,0 +1,14 @@
+// Student Name: Timothy Cassidy
+// Student Number: G00333333
+
+package main
+
+// format package import
+import "fmt"
+
+// Hello World main function
+func main() {
+	
+	
+	
+}// main
