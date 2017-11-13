686d7f27777fe7e454823c778d271b163a1810fe
Trojan-Hawk <G00333333@gmit.ie>
2017-11-13 21:44:22 +0000

    ElizaResponse function adapted to check if input contains 'father'

--- GoProblems3.go
+++ GoProblems3.go
@@ -3,14 +3,16 @@
 
 // Resources
 // https://golang.org/pkg/math/rand/#Rand.Intn
+// https://golang.org/pkg/regexp/
 // 
 
 package main
 
-// format package import
+// package imports
 import "fmt"
 import "time"
 import "math/rand"
+import "regexp"
 
 // generic responses string array
 var responseItems = []string {
@@ -21,12 +23,26 @@ var responseItems = []string {
 
 // function that takes a string and returns a response string
 func ElizaResponse(inputString string) string {
+	// MustCompile is like Compile but panics if the expression cannot be parsed. 
+	// It simplifies safe initialization of global variables holding compiled 
+	// regular expressions. 
+	fatherXp := regexp.MustCompile("(?i)\\bfather\\b")
+	
+
+	// FindStringIndex returns a two-element slice of integers defining the 
+	// location of the leftmost match in s of the regular expression. The match 
+	// itself is at s[loc[0]:loc[1]]. A return value of nil indicates no match. 
+	// in this case we are checking to see if the regxp "father" is contained within the input string
+	if len(fatherXp.FindStringIndex(inputString)) > 0 {
+		return "Why don't you tell me more about your father?"
+	}// if
+	
 	// Intn returns, as an int, a non-negative pseudo-random number
 	// using this number to randomly pick a response from the array
 	return responseItems[rand.Intn(len(responseItems))]
-}
+}// ElizaResponse
 
-// Hello World main function
+// Main function
 func main() {
 	// using the system clock to set a random seed value
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -38,7 +54,7 @@ func main() {
 	fmt.Println(ElizaResponse("Father was a teacher."))
 	fmt.Println("I was my father's favourite.")
 	fmt.Println(ElizaResponse("I was my father's favourite."))
-	fmt.Println("I'm looking forward to the weekend.")
+	fmt.Println("I am looking forward to the weekend.")
 	fmt.Println(ElizaResponse("I'm looking forward to the weekend."))
 	fmt.Println("My grandfather was French!")
 	fmt.Println(ElizaResponse("My grandfather was French!"))
