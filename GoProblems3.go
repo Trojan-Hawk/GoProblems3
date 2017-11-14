38c68a52c90cae84febedce4107f436d8ad442f6
Trojan-Hawk <G00333333@gmit.ie>
2017-11-14 13:32:37 +0000

    More inputs and responses added

--- GoProblems3.go
+++ GoProblems3.go
@@ -22,6 +22,14 @@ var responseItems = []string {
 	"Why do you say that?",
 }// responseItems
 
+// generic feeling responses string array
+var feelings = []string {
+	"very good.",
+	"not very good.",
+	"pretty good.",
+	"could be better.",
+}
+
 // function that takes a string and returns a response string
 func ElizaResponse(inputString string) string {
 	// MustCompile is like Compile but panics if the expression cannot be parsed. 
@@ -37,25 +45,59 @@ func ElizaResponse(inputString string) string {
 		return "Why don't you tell me more about your father?"
 	}// if
 	
-	regXp = regexp.MustCompile("(?i)i(?:'| a)?m(.*)")
+	// if the input begins with how are you
+	regXp = regexp.MustCompile("(?i)how are you(.*)")
 	subString := regXp.FindStringSubmatch(inputString)
 	
 	if len(subString) > 1 {
-		// replacing the full stop with a question mark
-		str := strings.Replace(subString[1], ".", "?", 1)
-		// reflecting the pronouns in the captured groups
-		str = strings.Replace(str, "you’re", "I'm", 1)
-		str = strings.Replace(str, "your", "my", 1)
-		str = strings.Replace(str, "you", "I", 1)
-		str = strings.Replace(str, "me", "you", 1)
-		
+		return feelings[rand.Intn(len(responseItems))]
+	}// if
+	
+	// if the input begins with how
+	regXp = regexp.MustCompile("(?i)how(.*)")
+	subString = regXp.FindStringSubmatch(inputString)
+	
+	if len(subString) > 1 {
+		str := tidyOutput(subString[1])
+		return "I don't know, how" + str	
+	}// if
+	
+	// if the input begins with “i am “, “I AM “, “I’m “, “Im “, “i’m “
+	regXp = regexp.MustCompile("(?i)i(?:'| a|)?m(.*)")
+	subString = regXp.FindStringSubmatch(inputString)
+	
+	if len(subString) > 1 {
+		str := tidyOutput(subString[1])
 		return "How do you know you are" + str
-	}
+	}// if
+	
+	// if the input begins with what
+	regXp = regexp.MustCompile("(?i)what(.*)")
+	subString = regXp.FindStringSubmatch(inputString)
+	
+	if len(subString) > 1 {
+		str := tidyOutput(subString[1])
+		return "What do you mean what" + str
+	}// if
 	
 	// Intn returns, as an int, a non-negative pseudo-random number
 	// using this number to randomly pick a response from the array
 	return responseItems[rand.Intn(len(responseItems))]
 }// ElizaResponse
+
+// function to tidy up the string to output
+func tidyOutput(str string) string {
+	// replacing the full stop with a question mark
+	str = strings.Replace(str, ".", "?", 1)
+	// reflecting the pronouns in the captured groups
+	str = strings.Replace(str, "you are", "I'm", 1)
+	str = strings.Replace(str, "you’re", "I'm", 1)
+	str = strings.Replace(str, "your", "my", 1)
+	str = strings.Replace(str, "you", "I", 1)
+	str = strings.Replace(str, "me", "you", 1)
+	str = strings.Replace(str, "i am", "you are", 1)
+	return str
+}// tidyOutput
 
 // Main function
 func main() {
@@ -73,6 +115,7 @@ func main() {
 	fmt.Println(ElizaResponse("I'm looking forward to the weekend."))
 	fmt.Println("My grandfather was French!")
 	fmt.Println(ElizaResponse("My grandfather was French!"))
+	fmt.Println()
 	
 	fmt.Println("I am happy.")
 	fmt.Println(ElizaResponse("I am happy."))
@@ -82,7 +125,34 @@ func main() {
 	fmt.Println(ElizaResponse("I am not sure that you understand the effect that your questions are having on me."))
 	fmt.Println("I am supposed to just take what you’re saying at face value?")
 	fmt.Println(ElizaResponse("I am supposed to just take what you’re saying at face value?"))
+	fmt.Println()
 	
 	// More input patterns
+	// what
+	fmt.Println("What do you think you are doing?")
+	fmt.Println(ElizaResponse("What do you think you are doing?"))
+	fmt.Println("What is life?")
+	fmt.Println(ElizaResponse("What is life?"))
+	fmt.Println("What is wubbalubbadubdub?")
+	fmt.Println(ElizaResponse("What is wubbalubbadubdub?"))
+	fmt.Println()
+	
+	// how are you
+	fmt.Println("How are you?")
+	fmt.Println(ElizaResponse("How are you?"))
+	fmt.Println("How are you feeling today?")
+	fmt.Println(ElizaResponse("How are you feeling today?"))
+	fmt.Println("How are your family doing?")
+	fmt.Println(ElizaResponse("How are your family doing?"))
+	fmt.Println()
+	
+	// how
+	fmt.Println("How do you build a car?")
+	fmt.Println(ElizaResponse("How do you build a car?"))
+	fmt.Println("How do you know what I am saying?")
+	fmt.Println(ElizaResponse("How do you know what I am saying?"))
+	fmt.Println("How can this be possiable?")
+	fmt.Println(ElizaResponse("How can this be possiable?"))
+
 	
 }// main
